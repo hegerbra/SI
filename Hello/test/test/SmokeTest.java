288a7f899b6e6bd179823c940b26ef1aaec18ab9
hegerba <hegerba@fit.cvut.cz>
2016-01-01 18:11:52 +0100

    first commit

---
+++ Hello/test/test/SmokeTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author patrik
+ */
+public class SmokeTest {
+
+    @Test
+    public void smoke() {
+        assertEquals(1, 1);
+    }
+
+}
